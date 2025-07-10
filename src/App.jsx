47e0b6ca11e69143b9ae54c674c1907cf8935661
anshitraj <anshitraj4@gmail.com>
2025-07-10 23:56:46 +0530

    Updated connection to use VITE_SOLANA_RPC_URL from .env

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import "./App.css";
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
 
 // wallet adapter imports
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
-} from '@solana/wallet-adapter-react-ui';
-import '@solana/wallet-adapter-react-ui/styles.css';
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
+  const endpoint = import.meta.env.VITE_SOLANA_RPC_URL;
   return (
-    <div style={{width: "100vw"}}>
-      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+    <div style={{ width: "100vw" }}>
+      <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={[]} autoConnect>
-            <WalletModalProvider>
-              <div style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                padding: 20
-              }}>
-                <WalletMultiButton />
-                <WalletDisconnectButton />
-              </div>
-              <TokenLaunchpad></TokenLaunchpad>
-            </WalletModalProvider>
+          <WalletModalProvider>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: 20,
+              }}
+            >
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <TokenLaunchpad></TokenLaunchpad>
+          </WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
